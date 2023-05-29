0f00e63d21d9964d23b7378eb3e67ecda8e2c4e1
rahgawirajakumaraswamy <rahgawiraja1999@outlook.de>
2023-05-29 19:06:04 +0200

    worked on pokemonjs

--- src/App.js
+++ src/App.js
@@ -2,12 +2,20 @@ import './App.css';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 import Home from './pages/Home';
+import { Route,Routes } from 'react-router-dom';
+import Pokemon from './pages/Pokemon';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Home />
+      <Routes>
+        <Route path='/pokemon' element= {<Home />} />
+        <Route path='/pokemon/:id' element= {<Pokemon />} />
+      
+      
+      
+      </Routes>
       <Footer />
     </div>
   );
